786577413263ac6bc0972130d33f74ce0c877095
TwnKey <feitaishi45@gmail.com>
2022-06-21 14:55:12 +0200

    adding u16array data type

--- lib/packer.py
+++ lib/packer.py
@@ -1,7 +1,7 @@
 from io import BufferedReader
 import struct
 from typing import Literal, Tuple, Union
-
+import math
 
 def writeint(
     stream: BufferedReader,
@@ -12,6 +12,21 @@ def writeint(
 ) -> int:
     return stream.write(value.to_bytes(size, endian, signed=signed))
 
+def writeintoffset(
+    stream: BufferedReader,
+    offset: int,
+    value: int,
+    size: int,
+    endian: Literal["little", "big"] = "little",
+    signed: bool = False,
+) -> int:
+    return_offset = stream.tell()
+    stream.seek(offset)
+    written_length = writeint(stream, value, size, endian, signed)
+    stream.seek(return_offset)
+    return written_length
+
+    
 
 def writefloat(stream: BufferedReader, value: float) -> int:
     return stream.write(struct.pack("<f", value))
@@ -60,6 +75,13 @@ def pack_data(
     data: int | str | float,
     extra_data_idx: int,
 ):
+
+	#I realized later that there was no padding in t_place, but I keep it as it might be useful (you can also remove it if you want)
+    to_be_aligned = False
+    if datatype.startswith("aligned_"):
+        to_be_aligned = True
+        datatype = datatype[8:]
+        
     if isinstance(datatype, dict):
         schema: dict = datatype["schema"]
         for i in range(datatype["size"]):
@@ -78,6 +100,20 @@ def pack_data(
             extra_data_idx += writetextoffset(stream, data, extra_data_idx)
         else:
             extra_data_idx += writetextoffset(
-                stream, data, extra_data_idx, encoding=datatype[8:]
+                stream, data, extra_data_idx, encoding=datatype[7:]
             )
+    elif datatype == "u16array":
+        writeint(stream, extra_data_idx, 8)
+        
+        writeint(stream, len(data), 4)
+        for i_u16 in range(0, len(data)):
+            writeintoffset(stream, extra_data_idx + 2 * i_u16, data[i_u16], 2)
+        extra_data_idx = extra_data_idx + 2 * len(data)
+        
+    #padding zeros to match the original file (essentially as safety measure)    
+    if (to_be_aligned):
+        to_be_padded = math.ceil(extra_data_idx / 4) * 4 - extra_data_idx 
+        writeintoffset(stream, extra_data_idx, 0, to_be_padded)
+        extra_data_idx = extra_data_idx + to_be_padded
+        
     return extra_data_idx
